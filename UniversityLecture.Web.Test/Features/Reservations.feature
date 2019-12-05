bcafba9364bda8a3f07a718510680e54129aa5f6
valerik <vhorkovoy@hotmail.com>
2019-12-05 16:32:28 +0200

    Add Specflow Acceptance Tests

---
+++ UniversityLecture.Web.Test/Features/Reservations.feature
@@ -0,0 +1,17 @@
+﻿Feature: Reservations
+	Checking if functionality of managing (create, delete) 
+	reservations is working as expected
+
+Scenario: Create reservation
+Given I am a user
+And I make a post request to 'api/Account' to get token with the following data 
+| Login | Password |
+| demo  | demo     |
+And the response status code is '200'
+And the response data should be '*'
+When I make a post request to 'api/reservations' with the following data 
+| LectureHallId | LecturerId | Date       | StartAt | EndAt |
+| 1             | 1          | 05.12.2019 | 08:00   | 11:00 |
+Then the response status code is '201'
+And the response data should be '{"id":*}'
+And I make delete request 'api/reservations' to delete created reservation
---
+++ UniversityLecture.Web/StartupTest.cs
@@ -0,0 +1,59 @@
+﻿using Autofac;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UniversityLecture.Repo;
+using UniversityLecture.WEB.Profiles;
+using UniversityLecture.WEB.Services;
+using UniversityLecture.WEB.Interfaces;
+
+
+namespace UniversityLecture.Web
+{
+    public class StartupTest
+    {
+        public StartupTest(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            new ConfigurationBuilder().Build();
+        }
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new SqlModule(Configuration.GetConnectionString("UL")));
+            builder.Register(context =>
+            {
+                return new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile<LectureHallProfile>();
+                    cfg.AddProfile<LecturerProfile>();
+                    cfg.AddProfile<SubjectProfile>();
+                    cfg.AddProfile<ReservationProfile>();
+
+                }).CreateMapper();
+            }).As<IMapper>().SingleInstance();
+            builder.RegisterType<AuthenticateService>().As<IAuthenticate>();
+        }
+    }
+}
