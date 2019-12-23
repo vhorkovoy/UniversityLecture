6dc8932e3dd482396340649865c123dc55442ec5
unknown <vhorkovoy@hotmail.com>
2019-12-23 15:19:17 +0200

    Add specification support

---
+++ UniversityLecture.Bll/Specifications/BaseSpec.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using UniversityLecture.Repo.Interfaces;
+
+namespace UniversityLecture.Bll.Specifications
+{
+    public abstract class BaseSpec<T> : ISpecification<T>
+    {
+        public BaseSpec(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+            Includes = new List<Expression<Func<T, object>>>();
+            IncludeStrings = new List<string>();
+        }
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; }
+        public List<string> IncludeStrings { get; }
+
+       
+        protected virtual void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+        // string-based includes allow for including children of children, e.g. Basket.Items.Product
+        protected virtual void AddInclude(string includeString)
+        {
+            IncludeStrings.Add(includeString);
+        }
+    }
+}
---
+++ UniversityLecture.Bll/Specifications/ReservationWithLectureSpec.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityLecture.Core;
+
+namespace UniversityLecture.Bll.Specifications
+{
+    public class ReservationWithLectureSpec : BaseSpec<Reservation>
+    {
+        public ReservationWithLectureSpec(int lecturerId):
+            base(r => r.LecturerId == lecturerId)
+        {
+            AddInclude(r => r.Lecturer);
+        }
+    }
+}
